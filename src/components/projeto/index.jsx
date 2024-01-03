94b375136d6fc2386fdd2f568a5c6b03b69e5a82
rayaneacacio <rray74307@gmail.com>
2024-01-03 15:54:36 -0300

    update

--- src/components/projeto/index.jsx
+++ src/components/projeto/index.jsx
@@ -14,11 +14,7 @@ export function Projeto({ img, img_mobile, name, description, repository, deploy
       
       <div>
         <p> { description } </p>
-        <div style={{ display: "flex", gap: "2rem" }}>
-          <span>JAVASCRIPT</span>
-          <span>REACT</span>
-        </div>
-
+        
         <div className="div_links">
           <a href={ repository } target="_blank" title="repositório" > <FaGithub size={ 40 } /> </a>
           <a href={ deploy } target="_blank" title="deploy" > <MdVisibility size={ 40 } /> </a>
